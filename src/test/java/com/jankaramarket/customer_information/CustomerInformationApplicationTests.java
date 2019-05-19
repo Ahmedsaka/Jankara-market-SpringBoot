ecd8ec2bef706efb3767bb5cd6644aa678fcff52
Ahmed Saka <ahmedsaka91@gmail.com>
2019-05-19 10:25:25 +0100

    First github springboot commit

---
+++ src/test/java/com/jankaramarket/customer_information/CustomerInformationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jankaramarket.customer_information;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CustomerInformationApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
